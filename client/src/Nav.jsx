5238906bb515ce92d1dac6874399307f4358f554
Ryan Kler Polinar <146166139+RyaneKyler@users.noreply.github.com>
2024-05-08 09:43:55 +0800

    Add   ViewCart and Nav

---
+++ client/src/Nav.jsx
@@ -0,0 +1,22 @@
+Nav.jsx
+
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+        <img src="" alt="Logo" />
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/product">Product</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
