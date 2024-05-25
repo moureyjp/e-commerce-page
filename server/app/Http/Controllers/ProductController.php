4f39cc9844cac66d54a71e256b50b15089374c7a
moureyjp <moureyjamesp@gmail.com>
2024-05-26 02:16:21 +0800

    update server

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -10,4 +10,36 @@ class ProductController extends Controller {
         $products = Product::all();
         return response()->json($products);
     }
+
+    public function store(Request $request) {
+        $data = $request->validate([
+            "name" => "required",
+            "description" => "required",
+            "price" => "required"
+        ]);
+
+        $result = Product::create($data);
+
+        return response()->json($result, 200);
+    }
+
+    public function update(Request $request, $id) {
+        $data = $request->validate([
+            "name" => "required",
+            "description" => "required",
+            "price" => "required"
+        ]);
+
+        $product = Product::find($id);
+        $product->update($data);
+
+        return response()->json($product, 200);
+    }
+
+    public function destroy($id) {
+        $product = Product::find($id);
+        $product->delete();
+
+        return response()->json($product, 200);
+    }
 }
