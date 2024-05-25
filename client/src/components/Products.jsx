41b6c40408dffa248b399605158804413c3e6905
moureyjp <moureyjamesp@gmail.com>
2024-05-26 01:30:14 +0800

    update files

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,21 +1,73 @@
-const Product = ({ product, addToCart }) => {
-  const { id, name, description, price } = product;
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Product from "./Product";
+
+const Products = () => {
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async (url) => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(url);
+        setProducts(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("error");
+      }
+    };
+
+    fetchProducts("http://127.0.0.1:8000/api/products");
+  }, []);
+
+  const addToCart = async (id) => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/cart", {
+        product_id: id,
+      });
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <li className="bg-white rounded-lg overflow-hidden shadow-lg ring-4 ring-red-500 ring-opacity-40 max-w-sm w-full">
-      <div className="p-4">
-        <h3 className="text-lg font-medium mb-2">{name}</h3>
-        <p className="text-gray-600 text-sm mb-4">{description}</p>
-        <div className="flex items-center justify-between">
-          <span className="font-bold text-lg">${price}</span>
-          <button
-            onClick={() => addToCart(id)}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+    <>
+      {showSuccessMessage && (
+        <div className="bg-green-500 py-2 px-4 rounded-md text-white text-center fixed bottom-14 right-4 flex gap-4">
+          <p>Success! Your product has been added to the cart.</p>
+          <span
+            className="cursor-pointer font-bold"
+            onClick={() => setShowSuccessMessage(false)}
           >
-            Add to Cart
-          </button>
+            <sup>X</sup>
+          </span>
         </div>
-      </div>
-    </li>
+      )}
+      {isLoading ? (
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-64 w-64 "></div>
+        </div>
+      ) : (
+        <div className="mt-8 xl:px-4 max-xl:w-[90%] mx-auto">
+          <ul className="grid grid-cols-4 gap-8 max-lg:grid-cols-3 max-[800px]:grid-cols-2 max-sm:grid-cols-1 justify-items-center">
+            {products?.map((product) => (
+              <Product
+                key={product.id}
+                product={product}
+                addToCart={addToCart}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
-export default Product;
+
+export default Products;
